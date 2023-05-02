b86df15d1606b7308a5c10ba2d86db61603c3cae
Alok Dwivedi <66508892+alokdwivedi103@users.noreply.github.com>
2023-05-02 10:41:51 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -38,7 +38,7 @@ const PostWidget = ({
     const primary = palette.primary.main;
 
     const patchLike = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+        const response = await fetch(`https://wuddle-social-backend.onrender.com/posts/${postId}/like`, {
             method: "PATCH",
             headers: {
                 Authorization: `Bearer ${token}`,
