e7ff114a0e97e42cf2d2ae13fbb676e3dfd1f127
Alok Dwivedi <66508892+alokdwivedi103@users.noreply.github.com>
2023-05-02 12:43:57 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -68,7 +68,7 @@ const PostWidget = ({
                         height="auto"
                         alt="post"
                         style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-                        src={`http://localhost:3001/assets/${picturePath}`}
+                        src={`https://wuddle-social-backend.onrender.com/assets/${picturePath}`}
                     />
                 </Link>
             )}
